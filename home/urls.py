c1adb10bb7c743ed40a347786a286b5201484deb
Demir Antay <demir99antay@gmail.com>
2020-08-06 08:04:17 +0300

    finished version 1.0.0

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # Index of the webapp (landing page)
     path('', views.index, name="index"),
+    # Under Construction
+    path('under_construction/', views.under_construction, name="under_construction"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# Main Imports
 
-# Create your views here.
+# Django Imports
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.files import File
+from django.contrib.auth.models import User
+
+# My Module Imports
+
+
+def index(request):
+    """index will redirect the page to `learn` if user is logged in"""
+    return render(request, "home/landing_page.html")
+
+
+def under_construction(request):
+    """under constrction page"""
+    return render(request, "redirect_pages/under_construction.html")
