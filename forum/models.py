7a52027b55ca12b3e6f3d451b7e980946a15bff4
Demir Antay <demir99antay@gmail.com>
2020-08-14 09:36:42 +0300

    finished the statics

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+# Main Imports
 
-# Create your models here.
+# Django Imports
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# My Module Imports
