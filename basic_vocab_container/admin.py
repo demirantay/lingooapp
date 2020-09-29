96762c9188015d546da2d29bb385d619a0ace735
Demir Antay <demir99antay@gmail.com>
2020-09-29 14:07:13 +0300

    added admin filters for admin panel

--- basic_vocab_container/admin.py
+++ basic_vocab_container/admin.py
@@ -3,6 +3,12 @@ from .models import BasicVocabularyContainer, StudentVocabProgress
 from .models import BasicVocabErrorReport
 
 # Register your models here.
-admin.site.register(BasicVocabularyContainer)
+# admin.site.register(BasicVocabularyContainer)
 admin.site.register(StudentVocabProgress)
 admin.site.register(BasicVocabErrorReport)
+
+
+@admin.register(BasicVocabularyContainer)
+class BasicVocabularyContainer(admin.ModelAdmin):
+    list_display = ('level', 'word', 'course',)
+    list_filter = ('level', 'course')
