6e106ccceb05036f8512a20ece16fe4b3939773c
Demir Antay <demir99antay@gmail.com>
2020-12-07 11:22:14 +0300

    finished category pages for ranking system

--- basic_ranking/views.py
+++ basic_ranking/views.py
@@ -79,7 +79,6 @@ def ranking_overview(request, page):
     for student in current_user_student_profiles:
         current_user_ranks[student.id] = list(all_student_profiles).index(student) + 1
 
-
     # Get all the ranks for the bottom
     rankings = {}
     rank = post_records_starting_point + 1
@@ -108,9 +107,102 @@ def ranking_overview(request, page):
 
 
 def category_ranking(request, language, page):
+    """
+    In this page the users can see the ranking of different language courses
+    """
+
+    # Deleting admin-typed user session
+    # Deleting programmer-typed-user session
+    # Deleting Teacher-typed user sessions
+
+    # ACCESS CONTROL
+    delete_teacher_user_session(request)
+
+    # Get the current users
+    current_basic_user = get_current_user(request, User, ObjectDoesNotExist)
+
+    current_basic_user_profile = get_current_user_profile(
+        request,
+        User,
+        BasicUserProfile,
+        ObjectDoesNotExist
+    )
+
+    # Getting the current teacher profile
+    current_teacher_profile = get_current_teacher_user_profile(
+        request,
+        User,
+        TeacherUserProfile,
+        ObjectDoesNotExist
+    )
+
+    # Get the current language of the page
+    try:
+        current_langauge = Language.objects.get(name=language)
+    except ObjectDoesNotExist:
+        current_langauge = None
+
+    if current_langauge == None:
+        return HttpResponseRedirect("/")
+
+    # Get all of the current students for navigation
+    try:
+        current_user_student_profiles = Student.objects.filter(
+            basic_user_profile=current_basic_user_profile
+        ).order_by("-xp")
+    except ObjectDoesNotExist:
+        current_user_student_profiles = None
+
+    # Get all of the students with the current language of the category
+    # At every page there will be 80 entries so always multiply it by that and
+    # then reduce your objects
+    current_page = page
+    previous_page = page-1
+    next_page = page+1
+
+    post_records_starting_point = current_page * 1000
+    post_records_ending_point = post_records_starting_point + 1000
+
+    try:
+        all_student_profiles = Student.objects.filter(
+            langauge=current_langauge
+        ).order_by("-xp")
+    except ObjectDoesNotExist:
+        all_student_profiles = None
+
+    filtered = all_student_profiles[post_records_starting_point:post_records_ending_point]
+
+    # Get all the ranks for the bottom
+    rankings = {}
+    rank = post_records_starting_point + 1
+    for student in filtered:
+        rankings[student.id] = rank
+        rank += 1
+
+    # Get the rank of the current student
+    for student in current_user_student_profiles:
+        if student.langauge.name == language:
+            current_student = student
+            break
+
+    current_student_rank = list(all_student_profiles).index(current_student) + 1
 
     data = {
-
+        "current_basic_user": current_basic_user,
+        "current_basic_user_profile": current_basic_user_profile,
+        "current_teacher_profile": current_teacher_profile,
+        "current_page": current_page,
+        "previous_page": previous_page,
+        "next_page": next_page,
+        "all_student_profiles": all_student_profiles,
+        "current_user_student_profiles": current_user_student_profiles,
+        "page_langauge": language,
+        "rankings": rankings,
+        "filtered": filtered,
+        "current_student_rank": current_student_rank,
     }
 
-    return render(request, "basic_ranking/category_ranking.html", data)
+    if current_basic_user == None:
+        return HttpResponseRedirect("/auth/login/")
+    else:
+        return render(request, "basic_ranking/category_ranking.html", data)
