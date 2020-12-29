1c98ae6474df660c0bd2e308dc44d0d21fe456d3
Demir Antay <demir99antay@gmail.com>
2020-12-29 12:31:46 +0300

    added new models

--- basic_notifications/admin.py
+++ basic_notifications/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import NotificationBase
+from .models import NotificationBase, AnnouncementNotification
+from .models import AnnouncementIsRead
 
 # Register your models here.
 admin.site.register(NotificationBase)
+admin.site.register(AnnouncementNotification)
+admin.site.register(AnnouncementIsRead)
--- basic_notifications/models.py
+++ basic_notifications/models.py
@@ -79,3 +79,40 @@ class NotificationBase(models.Model):
 
     def __str__(self):
         return "Notfied user: " + str(self.notified_user)
+
+
+# Announcement Notification Model
+# -------------------
+# These announcaments appear on the homepage of every user
+class AnnouncementNotification(models.Model):
+    id = models.AutoField(primary_key=True)
+    creation_date = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return "Announcement: " + str(self.title)
+
+
+# Announcement IS READ Model
+# -------------------
+# This model holds the records of the suers who have read the announcements and
+# dismissed them
+class AnnouncementIsRead(models.Model):
+    id = models.AutoField(primary_key=True)
+    creation_date = models.DateField(default=timezone.now)
+    announcement = models.ForeignKey(
+        AnnouncementNotification,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+    user_profile = models.ForeignKey(
+        BasicUserProfile,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+
+    def __str__(self):
+        return "Dismisser: " + str(self.user_profile)
