a5678998a27ba529ebe866b3983e1e0d477b7c53
Demir Antay <demir99antay@gmail.com>
2021-01-15 13:54:08 +0300

    added new models and views for the teacher_dictonary explore page

--- teacher_dictionary/models.py
+++ teacher_dictionary/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+# Main Imports
 
-# Create your models here.
+# Django Imports
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# My Module Imports
+from teacher_authentication.models import TeacherUserProfile
+
+
+# Teacher - Dictionary
+# ----------------
+# In this model the teacher users can create new dictionaries but to be honest
+# all of the dictionaries are created by the admin to avoid any spamming
+class TeacherDictionary(models.Model):
+    creation_date = models.DateField(default=timezone.now)
+    id = models.AutoField(primary_key=True)
+    dictionary_language = models.CharField(
+        max_length=300,
+        blank=True,
+        null=True
+    )
+    dictionary_speakers_language = models.CharField(
+        max_length=300,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return "Dict: " + self.dictionary_language +  \
+               "| Lang: " + self.dictionary_speakers_language
--- teacher_dictionary/views.py
+++ teacher_dictionary/views.py
@@ -7,18 +7,56 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 
 # My Module Imports
+from profile_settings.models import BasicUserProfile
+from teacher_authentication.models import TeacherUserProfile
+from utils.session_utils import get_current_user, get_current_user_profile
+from utils.session_utils import get_current_teacher_user_profile
+
+from .models import TeacherDictionary
 
 
 def teacher_dictionary_explore(request):
     """
-
+    in this view the teachers can see the listings of all of the dictionaries
     """
+    # Deleting admin-typed user session
+    # Deleting programmer-typed-user session
+
+    # Get the current users
+    current_basic_user = get_current_user(request, User, ObjectDoesNotExist)
+
+    current_basic_user_profile = get_current_user_profile(
+        request,
+        User,
+        BasicUserProfile,
+        ObjectDoesNotExist
+    )
+
+    # Getting the teacher profile
+    current_teacher_profile = get_current_teacher_user_profile(
+        request,
+        User,
+        TeacherUserProfile,
+        ObjectDoesNotExist
+    )
+
+    # Get all of the dictionaries
+    try:
+        all_dictionaries = TeacherDictionary.objects.all()
+    except ObjectDoesNotExist:
+        all_dictionaries = None
 
     data = {
-
+        "current_basic_user": current_basic_user,
+        "current_basic_user_profile": current_basic_user_profile,
+        "current_teacher_profile": current_teacher_profile,
+        "all_dictionaries": all_dictionaries,
     }
 
-    return render(request, "teacher_dictionary/explore.html", data)
+    if "teacher_user_logged_in" in request.session:
+        return render(request, "teacher_dictionary/explore.html", data)
+    else:
+        return HttpResponseRedirect("/")
 
 
 def teacher_dictionary_build(request):
